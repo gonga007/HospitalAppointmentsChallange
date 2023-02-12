a2081fe5c98aa127106b6a847172945cfe01bbf8
Goncalo Alves <gonga007@gmail.com>
2023-02-12 16:09:02 +0000

    solved the getTopSpecialities problem and exposed and endpoint that gives that data

--- src/main/java/com/challenge/challenge/AppointmentController.java
+++ src/main/java/com/challenge/challenge/AppointmentController.java
@@ -1,15 +1,15 @@
 package com.challenge.challenge;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequestMapping("/api/appointments")
@@ -39,5 +39,34 @@ public class AppointmentController {
             return ResponseEntity.ok().body(consultsMap);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("getTopSpecialties")
+    public ResponseEntity<List<TopSpeciality>> getTopSpecialties(){
+
+        List<String> patientsCountedForSpeciality = new ArrayList<>();
+        List<TopSpeciality> topSpecialities = new ArrayList<>();
+        List<Appointment> appointments = appointmentRepository.findAll();
+        for(Speciality speciality : Speciality.values()){
+            String spec = speciality.getName();
+            patientsCountedForSpeciality.clear();
+           for(Appointment appointment : appointments){
+               if(appointment.getSpeciality().getName().equals(spec)){
+                   String patientName = appointment.getPatient();
+                   if(patientsCountedForSpeciality.contains(patientName)){
+                       continue;
+                   }
+                   patientsCountedForSpeciality.add(patientName);
+               }
+           }
+            topSpecialities.add(new TopSpeciality(spec,patientsCountedForSpeciality.size()));
+        }
+        List<TopSpeciality> filteredTopSpecialist = topSpecialities.stream().
+                filter( topSpeciality -> (topSpeciality.getNumberOfPatients() > 2 )).collect(Collectors.toList());
+        if(!CollectionUtils.isEmpty(filteredTopSpecialist)){
+            return ResponseEntity.ok().body(filteredTopSpecialist);
+        }
+        return ResponseEntity.notFound().build();
+
     }
 }
---
+++ src/main/java/com/challenge/challenge/TopSpeciality.java
@@ -0,0 +1,32 @@
+package com.challenge.challenge;
+
+public class TopSpeciality {
+
+    private String specialityName;
+    private int numberOfPatients;
+
+    public TopSpeciality(String specialityName, int numberOfPatients) {
+        this.specialityName = specialityName;
+        this.numberOfPatients = numberOfPatients;
+    }
+    public TopSpeciality(){
+
+    }
+
+    public String getSpecialityName() {
+        return specialityName;
+    }
+
+    public void setSpecialityName(String specialityName) {
+        this.specialityName = specialityName;
+    }
+
+    public int getNumberOfPatients() {
+        return numberOfPatients;
+    }
+
+    public void setNumberOfPatients(int numberOfPatients) {
+        this.numberOfPatients = numberOfPatients;
+    }
+
+}
